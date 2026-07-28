c4784fab596113e7797c7eda6138b19525cda062
Baveega <baveega.k@gmail.com>
2026-07-28 15:17:11 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-base-200">
+      <button className="btn btn-primary">Hello daisyUI!</button>
+    </div>
+  );
+}
+
+
+export default App;
