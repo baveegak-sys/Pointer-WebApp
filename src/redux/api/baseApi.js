825b87973f1c3831aaf6a71ae26c937995ebcd2b
Dipra <dipradas5940@gmail.com>
2026-08-03 14:28:20 +1000

    [DONE] Login and OTP page design completed

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -4,7 +4,7 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
 
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "http://localhost:5001/api",
   }),
 
   endpoints: () => ({}),
